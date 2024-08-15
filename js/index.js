b4d17db408b56e3bdb386d3aa08a373a10c2f11d
fizza nayab <tab2baku@gmail.com>
2024-08-15 22:55:38 +0400

    upd

--- js/index.js
+++ js/index.js
@@ -1,10 +1,74 @@
-// Smooth Scrolling
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
-        e.preventDefault();
+// Initialize Slick Slider
+$(document).ready(function(){
+    $('.slider-container').slick({
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        adaptiveHeight: true,
+        autoplay: true,
+        autoplaySpeed: 2000
+    });
+});
 
+// Smooth scrolling for links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
         document.querySelector(this.getAttribute('href')).scrollIntoView({
             behavior: 'smooth'
         });
     });
 });
+
+// Lightbox initialization
+lightbox.option({
+    'resizeDuration': 200,
+    'wrapAround': true
+});
+
+
+$(document).ready(function(){
+    $('.slider-container').slick({
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        adaptiveHeight: true,
+        autoplay: true,
+        autoplaySpeed: 2000
+    });
+});
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Modal Image Preview
+    var galleryModal = document.getElementById('galleryModal');
+    galleryModal.addEventListener('show.bs.modal', function (event) {
+        var button = event.relatedTarget;
+        var imageUrl = button.getAttribute('data-bs-image');
+        var modalImage = document.getElementById('modalImage');
+        modalImage.src = imageUrl;
+    });
+});
+
+
+
+// Back to Top Button
+const backToTopBtn = document.getElementById("backToTopBtn");
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        backToTopBtn.style.display = "block";
+    } else {
+        backToTopBtn.style.display = "none";
+    }
+}
+
+backToTopBtn.addEventListener("click", function() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+});
+
+
