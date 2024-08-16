c1152a48d660f4d13a7bd7213b9fd548159885dc
fizza nayab <tab2baku@gmail.com>
2024-08-16 23:45:15 +0400

    upda

--- js/index.js
+++ js/index.js
@@ -71,4 +71,10 @@ backToTopBtn.addEventListener("click", function() {
     document.documentElement.scrollTop = 0;
 });
 
-
+document.addEventListener("DOMContentLoaded", function() {
+    // Simulate loading time
+    setTimeout(function() {
+        document.querySelector(".preloader").style.display = "none";
+        document.querySelector(".content").style.display = "block";
+    }, 3000); // Adjust the loading time as needed
+});
